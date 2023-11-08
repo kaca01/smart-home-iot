4e01f0f25ba82a07d6b25cd1e21b6d2fd36a0269
savic-a <anastasijas557@gmail.com>
2023-11-08 17:06:15 +0100

    Moved buzzer simulation to folder

--- buzzer/buzzer.py
+++ buzzer/buzzer.py
@@ -1,18 +1,18 @@
-import RPi.GPIO as GPIO
+# import RPi.GPIO as GPIO
 import time
 
-GPIO.setmode(GPIO.BCM)
+# GPIO.setmode(GPIO.BCM)
 buzzer_pin = 17
-GPIO.setup(buzzer_pin, GPIO.OUT)
+# GPIO.setup(buzzer_pin, GPIO.OUT)
 
 def buzz(pitch, duration):
     period = 1.0 / pitch
     delay = period / 2
     cycles = int(duration * pitch)
     for i in range(cycles):
-        GPIO.output(buzzer_pin, True)
-        time.sleep(delay)
-        GPIO.output(buzzer_pin, False)
+        # GPIO.output(buzzer_pin, True)
+        # time.sleep(delay)
+        # GPIO.output(buzzer_pin, False)
         time.sleep(delay)
 try:
     while True:
@@ -21,7 +21,9 @@ try:
             pitch = 440
             duration = 0.1
             buzz(pitch, duration)
+            print("Cuje see!!")
             time.sleep(1)
 except KeyboardInterrupt:
-    GPIO.cleanup()
+    pass
+    # GPIO.cleanup()
 
