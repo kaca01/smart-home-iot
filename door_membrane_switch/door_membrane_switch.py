f5a90134205143913584af8fdcba82414fc5fae0
savic-a <anastasijas557@gmail.com>
2023-11-09 17:28:49 +0100

    Changed code for real time

--- door_membrane_switch/door_membrane_switch.py
+++ door_membrane_switch/door_membrane_switch.py
@@ -29,16 +29,22 @@ C4 = 21
 # GPIO.setup(C4, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 # def readLine(line, characters):
+#     character = ''
 #     GPIO.output(line, GPIO.HIGH)
 #     if(GPIO.input(C1) == 1):
+#         character = characters[0]
 #         print(characters[0])
 #     if(GPIO.input(C2) == 1):
+#         character = characters[0]
 #         print(characters[1])
 #     if(GPIO.input(C3) == 1):
+#         character = characters[0]
 #         print(characters[2])
 #     if(GPIO.input(C4) == 1):
+#         character = characters[0]
 #         print(characters[3])
 #     GPIO.output(line, GPIO.LOW)
+#     return character
 
 def input_pin():
         for i in range(2, -1, -1):
@@ -49,14 +55,27 @@ def input_pin():
             else:
                 print(f"Incorrect PIN. {i} attempts remaining")
 
+# def input_pin():
+#     pin = ""
+
+#     for i in range(4):
+#         character = readLine(R1, ["1","2","3","A"])
+#         character = readLine(R2, ["4","5","6","B"])
+#         character = readLine(R3, ["7","8","9","C"])
+#         character = readLine(R4, ["*","0","#","D"])
+#         pin += character
+#         time.sleep(0.2)  # wainting for another input
+
+#     return pin
+
 # try:
-#     while True:
-#         # call the readLine function for each row of the keypad
-#         readLine(R1, ["1","2","3","A"])
-#         readLine(R2, ["4","5","6","B"])
-#         readLine(R3, ["7","8","9","C"])
-#         readLine(R4, ["*","0","#","D"])
-#         time.sleep(0.2)
+#    for i in range(2, -1, -1):
+#             pin = input_pin()
+#             if pin == expected_pin:
+#                 print("Correct PIN!")
+#                 break
+#             else:
+#                 print(f"Incorrect PIN. {i} attempts remaining")
 # except KeyboardInterrupt:
 #     print("\nApplication stopped!")
 
