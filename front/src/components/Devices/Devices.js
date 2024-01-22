4c7f7fdedfc0529b8c19ad0c63d178ad71b487df
kaca01 <96604086+kaca01@users.noreply.github.com>
2024-01-22 13:47:57 +0100

    Added grafana panel on frontend

--- front/src/components/Devices/Devices.js
+++ front/src/components/Devices/Devices.js
@@ -1,8 +1,10 @@
 import { Component } from "react";
 import './Devices.css';
+import Iframe from 'react-iframe';
 
 
 export class Devices extends Component {
+    grafanaGraphUrl = 'http://localhost:3000/d/dced5fb4-e93d-40d2-96bb-85bc7270a2d5/odbrana-3?orgId=1&from=1705923737720&to=1705927337720&viewPanel=1';
     constructor(props) {
         super(props);
     }
@@ -11,6 +13,7 @@ export class Devices extends Component {
         return (
             <div>
                 <p>Welcome to Devices page!</p>
+                <Iframe url={this.grafanaGraphUrl} width="100%" height="600px"/>
             </div>
         )
     }
