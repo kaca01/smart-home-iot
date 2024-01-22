0cf2ea106720c457cb7a2acab48ca12dfe25aae8
savic-a <anastasijas557@gmail.com>
2024-01-22 21:21:17 +0100

    Deleted some files and methods

--- devices/main.py
+++ devices/main.py
@@ -13,119 +13,25 @@ from lcd.lcd import run_lcd
 from segment_display.segment_display import run_4d7sd
 from infrared.infrared import run_infrared
 from gyroscope.gyroscope import run_gyroscope
-from devices.menu_prints import print_lights_menu, print_main_menu, print_door_sensors_menu, print_exit, print_room_sensors_menu
 import time
 
 try:
-    import RPi.GPIO as GPIO
-    GPIO.setmode(GPIO.BCM)
+        import RPi.GPIO as GPIO
+        GPIO.setmode(GPIO.BCM)
 except ModuleNotFoundError:
-    pass
+        pass
 
-
-# def handle_lights_menu():
-#     while True:
-#         print_lights_menu()
-#         inp = str(input(""))
-#         inp = inp.strip().lower()
-#         if inp == "0":
-#             break
-#         elif inp == "1":
-#             dl_settings = settings["DL"]
-#             run_dl(dl_settings, '1')
-#         elif inp == "2":
-#             dl_settings = settings["DL"]
-#             run_dl(dl_settings, '2')
-#         else:
-#             print("Invalid input!")
-#     main()
-
-
-# def handle_door_sensors_menu():
-#     while True:
-#         print_door_sensors_menu()
-#         inp = str(input(""))
-#         inp = inp.strip().lower()
-#         if inp == "0":
-#             break
-#         elif inp == "1":
-#             dus1_settings = settings["DUS1"]
-#             thread = threading.Thread(target=run_dus1, args=(dus1_settings, stop_event_dus1))
-#             thread.start()
-#         elif inp == "2":
-#             stop_event_dus1.set()
-#         elif inp == "3":
-#             dpir1_settings = settings["DPIR1"]
-#             thread = threading.Thread(target=run_dpir1, args=(dpir1_settings, stop_event_dpir1))
-#             thread.start()
-#         elif inp == "4":
-#             stop_event_dpir1.set()
-#         else:
-#             print("Invalid input!")
-#     main()
-
-
-# def handle_room_sensors_menu():
-#     while True:
-#         print_room_sensors_menu()
-#         inp = str(input(""))
-#         inp = inp.strip().lower()
-#         if inp == "0":
-#             break
-#         elif inp == "1":
-#             thread = threading.Thread(target=run_pir1, args=(settings["PIR1"], stop_event_pir1,))
-#             thread.start()
-#         elif inp == "2":
-#             stop_event_pir1.set()
-#         elif inp == "3":
-#             thread = threading.Thread(target=run_pir2, args=(settings["PIR2"], stop_event_pir2,))
-#             thread.start()
-#         elif inp == "4":
-#             stop_event_pir2.set()
-#         elif inp == "5":
-#             stop_event_dht1.clear()
-#             thread = threading.Thread(target=run_dht1, args=(settings["DHT1"], stop_event_dht1))
-#             thread.start()
-#         elif inp == "6":
-#             stop_event_dht1.set()
-#         elif inp == "7":
-#             stop_event_dht2.clear()
-#             thread = threading.Thread(target=run_dht2, args=(settings["DHT2"], stop_event_dht2))
-#             thread.start()
-#         elif inp == "8":
-#             stop_event_dht2.set()
-#         else:
-#             print("Invalid input!")
-
-#     main()
-
-
-# def main():
-#     while True:
-#         print_main_menu()
-#         inp = str(input(""))
-#         inp = inp.strip().lower()
-#         if inp == "0":
-#             for event in events:
-#                 event.set()
-#             print_exit()
-#             exit()
-#         elif inp == "1":
-#             ds1_settings = settings["DS1"]
-#             run_ds1(ds1_settings)
-#         elif inp == "2":
-#             handle_lights_menu()
-#         elif inp == "3":
-#             handle_door_sensors_menu()
-#         elif inp == "4":
-#             run_buzzer(settings['DB'])
-#         elif inp == "5":
-#             handle_room_sensors_menu()
-#         elif inp == "6":
-#             run_dms(settings["DMS"])
-#         else:
-#             print("Invalid input!")
-
+def print_exit():
+        print("""
+                        ***   ***
+                       ***** *****
+                        *********
+                         ******* 
+                          *****
+                           ***
+                            *
+                """)
+        print("Bye!")
 
 if __name__ == "__main__":
         print('Starting app')
